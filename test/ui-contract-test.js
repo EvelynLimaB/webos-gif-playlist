a970d6307b7b3814738d972a8b625faabf3796af
Evelyn Lima <104331636+EvelynLimaB@users.noreply.github.com>
2026-07-21 09:20:39 -0300

    test: cover in-app update controls and adapter methods

--- test/ui-contract-test.js
+++ test/ui-contract-test.js
@@ -4,6 +4,7 @@ var fs = require("fs");
 var path = require("path");
 var root = path.resolve(__dirname, "..");
 var html = fs.readFileSync(path.join(root, "index.html"), "utf8");
+var updater = fs.readFileSync(path.join(root, "js/update.js"), "utf8");
 var view = fs.readFileSync(path.join(root, "js/view.js"), "utf8");
 var app = fs.readFileSync(path.join(root, "js/app.js"), "utf8");
 var webos = fs.readFileSync(path.join(root, "js/webos.js"), "utf8");
@@ -19,9 +20,9 @@ while ((match = idPattern.exec(html)) !== null) {
 }
 
 var lookupPattern = /getElementById\("([A-Za-z0-9_-]+)"\)/g;
-while ((match = lookupPattern.exec(view)) !== null) {
+while ((match = lookupPattern.exec(view + "\n" + updater)) !== null) {
     if (!ids[match[1]]) {
-        throw new Error("view.js references missing HTML id: " + match[1]);
+        throw new Error("JavaScript references missing HTML id: " + match[1]);
     }
 }
 
@@ -34,10 +35,23 @@ while ((match = lookupPattern.exec(view)) !== null) {
     }
 });
 
-["init", "preflight", "status", "list", "addUrl", "remove", "move", "setOption", "applyTemporary", "enable", "disable", "reset", "testScreensaver"].forEach(function(name) {
+["init", "preflight", "status", "list", "addUrl", "remove", "move", "setOption", "applyTemporary", "enable", "disable", "reset", "getInstalledAppInfo", "fetchUpdateManifest", "installUpdate", "testScreensaver"].forEach(function(name) {
     if (webos.indexOf("WebOSService.prototype." + name) === -1) {
         throw new Error("missing WebOS adapter method: " + name);
     }
 });
+
+["btn-update-check", "btn-update-install", "btn-update-auto", "value-update-version", "update-detail"].forEach(function(id) {
+    if (!ids[id]) {
+        throw new Error("missing update UI id: " + id);
+    }
+});
+
+if (html.indexOf('<script src="js/update.js"></script>') === -1) {
+    throw new Error("update policy script is not loaded");
+}
+if (updater.indexOf("UpdatePolicy.validateManifest") === -1 || updater.indexOf("new UpdateController") === -1) {
+    throw new Error("in-app updater controller is incomplete");
+}
 
 console.log("UI contract test passed");
