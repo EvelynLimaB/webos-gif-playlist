cfabf5ca98e6de728010fce6a5a71f0bb5bf0047
Evelyn Lima <104331636+EvelynLimaB@users.noreply.github.com>
2026-07-20 23:27:45 -0300

    fix: apply playlist before tests and expose preflight

--- js/webos.js
+++ js/webos.js
@@ -1,7 +1,5 @@
-// Platform adapter for the Homebrew Channel root service.
-// Written for the Chromium 53-era browser used by webOS 4.x.
-
-var APP_DIR = window.location.pathname.replace(/\/[^/]+$/, '');
+// webOS 4 / Chromium 53 compatible platform adapter.
+var APP_DIR = window.location.pathname.replace(/\/[^/]+$/, "");
 var MANAGER_PATH = APP_DIR + "/assets/manager.sh";
 
 function WebOSService() {}
@@ -34,18 +32,23 @@ WebOSService.prototype.luna = function(service, params) {
 };
 
 WebOSService.prototype.exec = function(command) {
-    return this.luna("luna://org.webosbrew.hbchannel.service/exec", { command: command })
-        .then(function(response) {
-            return (response.stdoutString || "") + (response.stderrString || "");
-        });
+    return this.luna("luna://org.webosbrew.hbchannel.service/exec", {
+        command: command
+    }).then(function(response) {
+        return (response.stdoutString || "") + (response.stderrString || "");
+    });
 };
 
-WebOSService.prototype.manager = function(argumentsString) {
-    return this.exec('sh "' + MANAGER_PATH + '" ' + argumentsString);
+WebOSService.prototype.manager = function(argumentsText) {
+    return this.exec('sh "' + MANAGER_PATH + '" ' + argumentsText);
 };
 
 WebOSService.prototype.init = function() {
     return this.manager("init");
+};
+
+WebOSService.prototype.preflight = function() {
+    return this.manager("preflight");
 };
 
 WebOSService.prototype.status = function() {
@@ -57,11 +60,12 @@ WebOSService.prototype.list = function() {
 };
 
 WebOSService.prototype.addUrl = function(url) {
+    var encoded;
+
     if (typeof url !== "string" || !/^https?:\/\//i.test(url)) {
         return Promise.reject("Enter a direct http:// or https:// GIF URL.");
     }
 
-    var encoded;
     try {
         encoded = window.btoa(unescape(encodeURIComponent(url)));
     } catch (error) {
@@ -71,30 +75,43 @@ WebOSService.prototype.addUrl = function(url) {
     if (!/^[A-Za-z0-9+/=]+$/.test(encoded)) {
         return Promise.reject("The encoded URL was rejected.");
     }
+
     return this.manager("add '" + encoded + "'");
 };
 
 WebOSService.prototype.remove = function(id) {
-    if (!/^[A-Za-z0-9._-]+$/.test(id)) return Promise.reject("Invalid item id.");
+    if (!/^[A-Za-z0-9._-]+$/.test(id)) {
+        return Promise.reject("Invalid item id.");
+    }
     return this.manager("remove '" + id + "'");
 };
 
 WebOSService.prototype.move = function(id, direction) {
-    if (!/^[A-Za-z0-9._-]+$/.test(id)) return Promise.reject("Invalid item id.");
-    if (direction !== "up" && direction !== "down") return Promise.reject("Invalid direction.");
+    if (!/^[A-Za-z0-9._-]+$/.test(id)) {
+        return Promise.reject("Invalid item id.");
+    }
+    if (direction !== "up" && direction !== "down") {
+        return Promise.reject("Invalid direction.");
+    }
     return this.manager("move '" + id + "' " + direction);
 };
 
 WebOSService.prototype.setOption = function(key, value) {
-    var allowed = {
+    var validators = {
         mode: /^(ordered|shuffle)$/,
-        duration: /^\d{4,6}$/,
+        duration: /^\d{5,6}$/,
         fit: /^(crop|fit|stretch)$/
     };
-    if (!allowed[key] || !allowed[key].test(String(value))) {
+
+    if (!validators[key] || !validators[key].test(String(value))) {
         return Promise.reject("Invalid setting.");
     }
+
     return this.manager("set " + key + " '" + value + "'");
+};
+
+WebOSService.prototype.applyTemporary = function() {
+    return this.manager("apply");
 };
 
 WebOSService.prototype.enable = function() {
@@ -105,19 +122,21 @@ WebOSService.prototype.disable = function() {
     return this.manager("disable");
 };
 
-WebOSService.prototype.uninstallOverride = function() {
-    return this.manager("uninstall");
-};
-
 WebOSService.prototype.reset = function() {
     return this.manager("reset");
 };
 
-WebOSService.prototype.testScreensaver = function() {
+WebOSService.prototype.triggerScreensaver = function() {
     return this.exec(
         "luna-send -n 1 luna://com.webos.applicationManager/launch " +
-        "'{\"id\":\"com.webos.app.home\"}'" +
-        " && sleep 3" +
-        " && luna-send -n 1 luna://com.webos.service.tvpower/power/turnOnScreenSaver '{}'"
+        "'{\"id\":\"com.webos.app.home\"}' && sleep 3 && " +
+        "luna-send -n 1 luna://com.webos.service.tvpower/power/turnOnScreenSaver '{}'"
     );
+};
+
+WebOSService.prototype.testScreensaver = function() {
+    var self = this;
+    return this.applyTemporary().then(function() {
+        return self.triggerScreensaver();
+    });
 };
